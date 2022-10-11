03cc5858f039a346c6d156d59e159303f77773f2
HeyArnoldo <joao.souza@colegiounion.edu.pe>
2022-10-11 15:33:30 -0500

    Update

--- index.js
+++ index.js
@@ -18,25 +18,37 @@ create({
 function start(client) {
   client.onMessage(async message => {
     if (message.type === "image"){
+        await client.sendText(message.from , "✅ Imagen recibida, convirtiendo para sticker...");
         client.simulateTyping(message.from, true)
         const mediaData = await decryptMedia(message);
         const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
         'base64'
         )}`;
-        await client.sendImageAsSticker(message.from, imageBase64, {
-          author: "💻",
-          pack: "🖥"
-        });
+        try {
+          await client.sendImageAsSticker(message.from, imageBase64, {
+            author: "💻",
+            pack: "🖥"
+          });
+        } catch (error) {
+          await client.sendText(message.from, "❌ Error al convertir la imagen en sticker (Intenta con otra imagen)");
+          await client.sendText(message.from, "❌ Error: " + error);
+        }
     }else if(message.type === "video"){
       client.simulateTyping(message.from, true)
+      await client.sendText(message.from , "✅ Video recibido, convirtiendo para sticker...");
       const mediaData = await decryptMedia(message);
       const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString(
       'base64'
       )}`;
-      await client.sendMp4AsSticker(message.from, imageBase64, {}, {
-        author: "💻",
-        pack: "🖥"
-      });
+      try {
+        await client.sendMp4AsSticker(message.from, imageBase64, {}, {
+          author: "💻",
+          pack: "🖥"
+        });  
+      } catch (error) {
+        await client.sendText(message.from, "❌ Error al convertir el video en sticker (Intenta con otro video)");
+        await client.sendText(message.from, "❌ Error: " + error);
+      }  
     }
   });
 }
